8d35b8907eeda0d65eea5497c94c120153148080
Mozart Mendes <mozartmaiafilho@hotmail.com>
2025-11-06 00:23:22 -0300

    commit desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "modelo 1", imei:"11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "7890", modelo: "modelo 2", imei: "2222222", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("whatsapp");
